f73cd1aaa1a158df718ee63f0a7c5b30f1d253b0
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-14 14:42:05 -0400

    add variable enviroment example and config

--- app/settings/enviroment.py
+++ app/settings/enviroment.py
@@ -1,11 +1,22 @@
+from enum import StrEnum
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class AvailableDatabases(StrEnum):
+    sqlite = "sqlite"
+    mysql = "mysql"
+
+
 class DatabaseSettings(BaseSettings):
+    type: AvailableDatabases
     host: str
     port: int
     user: str
     password: str
-    name: str
+    name_db: str
 
     model_config = SettingsConfigDict(env_prefix="DB_")
+
+
+database_settings = DatabaseSettings()
