02497bf3954b3152eb0d99c245e4607c57477302
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-17 20:49:53 -0400

    creación de registro y login (Servicio) de UserService

---
+++ src/app/services/__init__.py
@@ -0,0 +1,4 @@
+from .token_jwt import TokenJWTService
+from .user import UserService
+
+__all__ = ["UserService", "TokenJWTService"]
---
+++ src/app/services/user.py
@@ -0,0 +1,34 @@
+from pwdlib import PasswordHash
+
+from app.dto import UserDTO
+from app.entities import User
+from app.repository.interfaces import UserRepositoryProtocol
+from app.schemas import CredencialsUser
+
+
+class UserService:
+    def __init__(self, repository: UserRepositoryProtocol):
+        self.repository = repository
+        self.password_hash = PasswordHash.recommended()
+
+    def create_user(self, data: UserDTO) -> User:
+        # Encriptación de contrasenia
+        data.password = self.password_hash.hash(data.password)
+
+        # Creación del usuario en la db
+        user_id = self.repository.create(data)
+        user = self.repository.get_by_id(user_id)
+        if user is None:
+            raise Exception("El usuario no a podido ser creado")
+        return user
+
+    def get_user_by_credencials(self, credencials: CredencialsUser) -> User:
+        user = self.repository.get_by_email(credencials.email)
+
+        if user is None:
+            raise Exception("User with email ... not found")
+
+        # Comparación de contrasenia de las credenciales con la db
+        if self.password_hash.verify(user.password, credencials.password):
+            return user
+        raise Exception("Credenciales incorrectas")
