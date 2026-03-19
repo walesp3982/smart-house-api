b64d0b694b79916a080273a2e15ea730a8b8f4a3
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-19 19:57:16 -0400

    fix: /token incorrecto valor

--- src/app/routes/auth.py
+++ src/app/routes/auth.py
@@ -3,12 +3,13 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.depends import UserServiceDep
+from app.depends import TokenJWTServiceDep, UserServiceDep
 from app.exceptions import (
     CredencialsUserIncorrectError,
     UserNotFoundByEmailError,
 )
 from app.schemas import CredencialsUser
+from app.schemas.auth import Token
 
 router = APIRouter()
 
@@ -17,6 +18,7 @@ router = APIRouter()
 def token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     user_service: UserServiceDep,
+    jwt_service: TokenJWTServiceDep,
 ):
     # Creamos las credenciales
     credencials = CredencialsUser(
@@ -27,11 +29,14 @@ def token(
     # Inicializamos sesion con el usuario
     try:
         user = user_service.get_user_by_credencials(credencials)
-        return {"access_token": user.email, "token_type": "bearer"}
+
+        token = jwt_service.encode(user.id, user.name)
+        return Token(access_token=token, token_type="Bearer")
     except UserNotFoundByEmailError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Credenciales incorrectas",
+            headers={"WWW-Authenticate": "Bearer"},
         )
 
     except CredencialsUserIncorrectError:
