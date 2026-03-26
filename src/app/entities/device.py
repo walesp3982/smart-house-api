deb253a333df5a11bba76c41f53262e249394d2e
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-26 08:47:23 -0400

    se agregó actions de dispositivos para mqtt-broker

--- src/app/entities/device.py
+++ src/app/entities/device.py
@@ -1,6 +1,35 @@
+import copy
+from dataclasses import dataclass
 from enum import StrEnum
 
 from pydantic import BaseModel
+
+
+@dataclass
+class Action:
+    """
+    Es una representación de una action del device
+    en el MQTT broker
+    """
+
+    naming_broker: str
+    actions: list[str]
+
+
+ON_ACTION = Action(
+    "on",
+    ["prender", "prende", "encender", "enciende", "activar", "activa"],
+)
+
+OFF_ACTION = Action(
+    "off",
+    ["apaga", "apagar", "desactivar", "desactiva"],
+)
+
+VALUE_ACTION = Action(
+    "value",
+    ["valor", "estado"],
+)
 
 
 class DeviceType(StrEnum):
@@ -9,6 +38,47 @@ class DeviceType(StrEnum):
     CAMERA = "camera"
     LOCK = "lock"
     SENSOR = "sensor"
+
+    def validate_commands(self) -> list[Action]:
+        """
+        Va a dar los comandos validos en spanish
+        que puede realizar el device
+        """
+        match self:
+            case self.LIGHT:
+                return [
+                    ON_ACTION,
+                    OFF_ACTION,
+                ]
+            case self.THERMOSTAT:
+                own_value_action = copy.deepcopy(VALUE_ACTION)
+                own_value_action.actions.append("temperature")
+                return [
+                    ON_ACTION,
+                    OFF_ACTION,
+                    own_value_action,
+                ]
+            case self.CAMERA:
+                return [
+                    ON_ACTION,
+                    OFF_ACTION,
+                ]
+            case self.LOCK:
+                own_off_action = copy.deepcopy(OFF_ACTION)
+                own_off_action.actions.append("cerrar")
+                own_off_action.actions.append("cierra")
+                own_on_action = copy.deepcopy(ON_ACTION)
+                own_on_action.actions.append("abrir")
+                own_on_action.actions.append("abre")
+                return [
+                    own_off_action,
+                    own_on_action,
+                ]
+            case self.SENSOR:
+                return [
+                    ON_ACTION,
+                    OFF_ACTION,
+                ]
 
 
 class DeviceEntity(BaseModel):
