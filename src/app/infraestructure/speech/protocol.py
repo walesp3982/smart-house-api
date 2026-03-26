89529051e3925f45cd3860eb92b0099c8e77b2cf
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-26 08:22:43 -0400

    creamos proveedores de traduction audio-text y creamos un convertido a .wav

---
+++ src/app/infraestructure/speech/protocol.py
@@ -0,0 +1,11 @@
+# speech/protocol.py
+from typing import Protocol, runtime_checkable
+
+
+@runtime_checkable
+class SpeechRecognizerProtocol(Protocol):
+    """
+    Protocolo para transcribir wav a texto
+    """
+
+    def transcribe(self, wav_bytes: bytes) -> str: ...
