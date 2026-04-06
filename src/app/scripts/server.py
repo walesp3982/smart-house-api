24b076706516a31bb22ab376a256a776af261e36
Walter Espejo Linux <josuewalterespejoguarachi@gmail.com>
2026-04-06 17:53:33 -0400

    new script para uvicorn y agregamos port 0.0.0.0 (para red privada)

--- src/app/scripts/server.py
+++ src/app/scripts/server.py
@@ -3,7 +3,7 @@ import subprocess
 
 def dev():
     try:
-        subprocess.run(["fastapi", "dev", "src/app/main.py"])
+        subprocess.run(["fastapi", "dev", "src/app/main.py", "--host", "0.0.0.0"])
     except KeyboardInterrupt:
         pass
 
@@ -11,5 +11,14 @@ def dev():
 def prod():
     try:
         subprocess.run(["fastapi", "run", "src/app/main.py", "--host", "0.0.0.0"])
+    except KeyboardInterrupt:
+        pass
+
+
+def uvicorn_dev():
+    try:
+        subprocess.run(
+            ["uvicorn", "src.app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+        )
     except KeyboardInterrupt:
         pass
